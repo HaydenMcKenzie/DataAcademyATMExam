5e9006994793481e9f1569d7bef5a83ab51d00be
HaydenMcKenzie <haydendanielmcKenzie@gmail.com>
2023-02-23 11:09:08 +1100

    Setup Files Completed

--- project_ATMExam/setup.py
+++ project_ATMExam/setup.py
@@ -1,12 +1,3 @@
 from setuptools import setup
 
-setup(
-    name='project_ATMExam',
-    version='',
-    packages=['setuptools', ''],
-    url='',
-    license='MIT License',
-    author='Hayden',
-    author_email='',
-    description=''
-)
+setup()
